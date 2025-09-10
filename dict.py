a6030d2c61e790dbc90fff22bd3091088f2fe28f
yasse-me1 <misookimm61@gmail.com>
2025-09-10 16:33:58 +0500

    Add files via upload
    dictionary

---
+++ dict.py
@@ -0,0 +1,46 @@
+#1
+# dict1 = {
+#     "1":"ona-tili",
+#     "2":"matematika",
+#     "3":"ingliz-tili"
+#     }
+# print(dict1)
+#2
+# dict1 = {
+# "1":"ona-tili",
+# "2":"matematika",
+# "3":"ingliz-tili"
+# }
+# key = input("Kalitni kiriting: ")
+# qiymat = dict1.get(key)
+# print(qiymat)
+#3
+dict1 = {
+"1":"ona-tili",
+"2":"matematika",
+"3":"ingliz-tili"
+}
+print(dict1.pop("2"))
+print(dict1)
+#4
+dict2 = {
+    1:"CSS",
+    2:"JS",
+    3:"PYTHON",
+    4:"HTML"
+}
+print(dict2.keys())
+print(dict2.values())
+#5
+dict2 = {
+    1:"CSS",
+    2:"JS",
+    3:"PYTHON",
+    4:"HTML"
+}
+for i in dict2.keys():
+    print(dict2[i])
+
+
+
+ 
